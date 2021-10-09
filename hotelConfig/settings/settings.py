8a6acae6525ede24e316460675f9a03850ec0237
Victor Turamant <60545309+turamant@users.noreply.github.com>
2021-10-09 11:58:10 +0300

    Update settings.py
    change secret_key

--- hotelConfig/settings/settings.py
+++ hotelConfig/settings/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a+33c#+)mqu_5jf(zc#!qkejkuyf200=+&p!=k@%rz_^jct-t5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
